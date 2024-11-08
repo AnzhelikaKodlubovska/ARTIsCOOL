6568fece828fb35747f99fe2a62a701fc5b6a600
AnzhelikaKodlubovska <angelikagymnasticschannel@gmail.com>
2024-11-08 14:59:45 +0200

    part №3

--- projectdjango/main/urls.py
+++ projectdjango/main/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('about', views.about, name='about'),
-    path('contacts', views.contacts, name='contacts')
+    path('contacts', views.contacts, name='contacts'),
+    path('registration', views.registration, name='registration')
 ]
 
